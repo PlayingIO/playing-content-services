360f33015fb0c4f7c33099b9b24cf6216f1adcc6
forafish <fora.luo@gmail.com>
2017-07-19 17:20:33 +0800

    Fix populateByService and make it functional

--- src/helpers.js
+++ src/helpers.js
@@ -1,24 +1,33 @@
-import { flatten, groupBy, map, omit } from 'lodash';
+import fp from 'lodash/fp';
 import { plural } from 'pluralize';
 
-export function populateByService(app, documents, idField, typeField, options) {
-  let types = groupBy(documents, typeField);
+const populateList = (list, idField) => (data) => {
+  return fp.map((doc) => {
+    let item = data.find((item) => {
+      return String(doc[idField]) === String(item.id);
+    });
+    return item;
+  })(list);
+};
+
+const populateByService = (app, idField, typeField, options) => (list) => {
+  let types = fp.groupBy(typeField, list);
   return Promise.all(
     Object.keys(types).map((type) => {
       let entries = types[type];
       return app.service(plural(type)).find(Object.assign({
         query: {
-          _id: { $in: map(entries, idField) },
+          _id: { $in: fp.map(idField, entries) },
         }
       }, options));
     })
   ).then((results) => {
-    results = options.provider? flatten(map(results, 'data')) : flatten(results);
-    documents = map(documents, (doc) => {
-      return Object.assign({ _id: doc.id }, doc, results.find((item) => {
-        return String(doc[idField]) === String(item.id);
-      }));
-    });
-    return documents;
+    return fp.flow(
+      options.provider? fp.flow(fp.map('data'), fp.flatten) : fp.flatten,
+      populateList(list, idField),
+      fp.compact
+    )(results);
   });
-}
+};
+
+export default { populateList, populateByService };
