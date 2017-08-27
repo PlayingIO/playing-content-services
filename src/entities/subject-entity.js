6dbcedd0e3bd154bcdc6dbdab22230e12b739e18
forafish <fora.luo@gmail.com>
2017-08-27 23:53:41 +0800

    move subject to playing-vocabulary-services

--- src/entities/subject-entity.js
+++
@@ -1,7 +0,0 @@
-import Entity from 'mostly-entity';
-
-const SubjectEntity = new Entity('Subject');
-
-SubjectEntity.excepts('createdAt', 'updatedAt', 'destroyedAt');
-
-export default SubjectEntity.asImmutable();
