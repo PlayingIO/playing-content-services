1e1f624d8a3fb2c90d8f0dd23edf141db525fc80
forafish <fora.luo@gmail.com>
2017-09-01 18:40:26 +0800

    dublincore document model comment

--- src/models/document-model.js
+++ src/models/document-model.js
@@ -7,30 +7,34 @@ const options = {
   discriminatorKey: 'type'
 };
 
+/**
+ * Loosely based on the dublincore DCMI schema
+ * http://dublincore.org/documents/dcmi-terms/
+ */
 const fields = {
-  title: { type: 'String', required: true },
-  description: { type: 'String' },
-  parent: { type: 'ObjectId' },
+  title: { type: 'String', required: true },  // name given to the resource
+  description: { type: 'String' },            // description of the resource.
+  parent: { type: 'ObjectId' },               // parent resource
   path: { type: 'String', default: '/', unique: true },
-  subjects: [{ type: 'String' }],
-  tags: [{ type: 'String', unique: true }],
-  rights: [{ type: 'String' }],
-  source: { type: 'String' },
-  nature: { type: 'String' },
-  coverage: { type: 'String' },
-  issued: { type: 'Date' },
-  valid: { type: 'Date' },
-  expired: { type: 'Date' },
-  format: { type: 'String' },
-  language: { type: 'String' },
-  author: { type: 'String' },    // real author of the document
-  creator: { type: 'ObjectId' }, // creator of the document
-  contributors: [{ type: 'ObjectId' }], // contributor of the document
-  file: blob,   // main blob
-  files: blobs, // attachements
-  verion: { type: 'Number' },
-  locker: { type: 'ObjectId' },  // lock owner
-  lockedAt: { type: 'Date' }     // locked time
+  subjects: [{ type: 'String' }],             // the topic of the resource
+  tags: [{ type: 'String', unique: true }],   // the tags of the resource
+  rights: [{ type: 'String' }],               // information about rights held in and over the resource.
+  source: { type: 'String' },                 // related resource from which the described resource is derived.
+  nature: { type: 'String' },                 // nature or genre of the resource
+  coverage: { type: 'String' },               // spatial or temporal topic of the resource
+  issued: { type: 'Date' },                   // date issued
+  valid: { type: 'Date' },                    // start date of validity of a resource
+  expired: { type: 'Date' },                  // end date of validity of a resource
+  format: { type: 'String' },                 // file format, physical medium, or dimensions of the resource.
+  language: { type: 'String' },               // language of the resource.
+  author: { type: 'String' },                 // real author of the document
+  creator: { type: 'ObjectId' },              // creator of the document
+  contributors: [{ type: 'ObjectId' }],       // contributor of the document
+  file: blob,                                 // main blob of the resource
+  files: blobs,                               // attachements of the resource
+  verion: { type: 'Number' },                 // version of the resource
+  locker: { type: 'ObjectId' },               // lock owner
+  lockedAt: { type: 'Date' }                  // locked time
 };
 
 export default function(app, name) {
