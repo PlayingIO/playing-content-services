1e1fcb87a87c7fc086172e5c1f0b8fe59812a8da
forafish <fora.luo@gmail.com>
2017-11-27 15:58:56 +0800

    rename isDocumentType

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,13 +1,13 @@
 import computePath from './compute-path';
 import documentEnrichers from './document-enrichers';
 import fetchBlobs from './fetch-blobs';
-import isDocument from './is-document';
+import isDocumentType from './is-document-type';
 import presentDocument from './present-document';
 
 export {
   computePath,
   documentEnrichers,
   fetchBlobs,
-  isDocument,
+  isDocumentType,
   presentDocument
 };
