f6c1fc2e617caa6cc395673777d994e3e29d2e97
forafish <fora.luo@gmail.com>
2018-03-11 02:00:10 +0800

    add compute-ancestors hook func

---
+++ src/hooks/compute-ancestors.js
@@ -0,0 +1,27 @@
+import makeDebug from 'debug';
+import fp from 'mostly-func';
+import slug from 'limax';
+import path from 'path';
+import shortid from 'shortid';
+import { getParentDocument } from '../helpers';
+
+const debug = makeDebug('playing:content-services:hooks:computeAncestors');
+
+// compute ancestors of current document
+export default function computeAncestors(options = { slug: false }) {
+  return (hook) => {
+    const svcDocuments = hook.app.service('documents');
+
+    // get parent or root document
+    return getParentDocument(hook.data.path, hook.data.parent).then(parent => {
+      if (parent && parent.ancestors) {
+        // join the parent ancestors (against parent changing)
+        hook.data.ancestors = fp.concat(parent.ancestors, parent.id);
+      } else {
+        debug('Parent ancestors undefined', parent);
+        throw new Error('Parent ancestors undefined');
+      }
+      return hook;
+    });
+  };
+}
