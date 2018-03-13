5ca6a9adbfc98810bc78841a96536b78be82a7c9
forafish <fora.luo@gmail.com>
2018-03-14 00:44:39 +0800

    clean getParentAces code

--- src/helpers.js
+++ src/helpers.js
@@ -67,35 +67,37 @@ export const getParentAces = (app, docs, select = 'user,creator,*') => {
     return fp.map(helpers.typedId, doc.ancestors || []);
   }, docs));
 
+  const getAncestors = (ids) => ids.length > 0?
+    helpers.findWithTypedIds(app, ids) : Promise.resolve([]);
   const getAncestorPermissions = (ids) => ids.length > 0?
     svcPermissions.find({
       query: { subject: { $in: ids }, $select: select },
       paginate: false
     }) : Promise.resolve([]);
-  const getAncestors = (ids) => ids.length > 0?
-    helpers.findWithTypedIds(app, ids) : Promise.resolve([]);
-  
+
   return Promise.all([
-    getAncestorPermissions(typedIds),
     getAncestors(typedIds),
-  ]).then(([permits, ancestors]) => {
+    getAncestorPermissions(typedIds)
+  ]).then(([ancestors, permits]) => {
     const permissions = permits.data || permits;
     return fp.reduce((arr, doc) => {
+      if (!doc.ancestors) return arr;
       arr[doc.id] = [];
-      for (let i = (doc.ancestors || []).length - 1; i >= 0; i--) {
-        if (doc.ancestors[i]) {
-          const ancestor = fp.find(
-            fp.propEq('id', helpers.getId(doc.ancestors[i])), ancestors);
-          const ancestorAces = fp.filter(
-            fp.propEq('subject', helpers.typedId(doc.ancestors[i])), permissions);
-          if (ancestorAces.length > 0) {
-            // subject change to current doc
-            const aces = fp.map(fp.assoc('subject', helpers.typedId(doc)), ancestorAces);
-            arr[doc.id] = arr[doc.id].concat(aces);
-          }
-          // continue to get inherited permissions or not
-          if (!ancestor || !ancestor.inherited) break;
+      // acnestor reverse loop to check inheritance
+      for (let i = doc.ancestors.length - 1; i >= 0; i--) {
+        const ancestor = fp.find(
+          fp.propEq('id', helpers.getId(doc.ancestors[i])), ancestors);
+        if (!ancestor) break;
+
+        const ancestorAces = fp.filter(
+          fp.propEq('subject', helpers.typedId(doc.ancestors[i])), permissions);
+        if (ancestorAces.length > 0) {
+          // subject change to current doc
+          const aces = fp.map(fp.assoc('subject', helpers.typedId(doc)), ancestorAces);
+          arr[doc.id] = arr[doc.id].concat(aces);
         }
+        // continue to next inherited
+        if (!ancestor.inherited) break;
       }
       return arr;
     }, {}, docs);
