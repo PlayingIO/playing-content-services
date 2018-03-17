ef08db03371f85f2063ddf2dda15729a69e626e1
forafish <fora.luo@gmail.com>
2018-03-17 23:42:51 +0800

    add cache to author

--- src/services/author/author-hooks.js
+++ src/services/author/author-hooks.js
@@ -1,15 +1,19 @@
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
+
 import AuthorEntity from '~/entities/author-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        hooks.authenticate('jwt', options.auth)
+        hooks.authenticate('jwt', options.auth),
+        cache(options.cache)
       ]
     },
     after: {
       all: [
+        cache(options.cache),
         hooks.presentEntity(AuthorEntity, options),
         hooks.responder()
       ]
