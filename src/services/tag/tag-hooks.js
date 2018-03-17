271e4fced55efbbd09bd375198cb3cc472d6ef3a
forafish <fora.luo@gmail.com>
2018-03-17 23:42:35 +0800

    add cache to tag

--- src/services/tag/tag-hooks.js
+++ src/services/tag/tag-hooks.js
@@ -1,15 +1,19 @@
 import { hooks } from 'mostly-feathers-mongoose';
+import { cache } from 'mostly-feathers-cache';
+
 import TagEntity from '~/entities/tag-entity';
 
 module.exports = function(options = {}) {
   return {
     before: {
       all: [
-        hooks.authenticate('jwt', options.auth)
+        hooks.authenticate('jwt', options.auth),
+        cache(options.cache)
       ]
     },
     after: {
       all: [
+        cache(options.cache),
         hooks.presentEntity(TagEntity, options),
         hooks.responder()
       ]
