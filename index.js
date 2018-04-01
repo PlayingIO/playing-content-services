3d4666683201e2e1e403e5ff3584c7279c25b363
forafish <fora.luo@gmail.com>
2018-04-02 03:10:07 +0800

    add time log for esm loader

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require = require("esm")(module/*, options*/);
+console.time('playing-content-services import');
 module.exports = require('./src/index').default;
 module.exports.DocTypes = require('./src/constants').DocTypes;
 module.exports.entities = require('./src/entities').default;
 module.exports.hooks = require('./src/hooks');
 module.exports.models = require('./src/models').default;
+console.timeEnd('playing-content-services import');
