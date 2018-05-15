93c0a531bee3ff72c453341280f63ac089906077
forafish <fora.luo@gmail.com>
2018-05-15 17:12:56 +0800

    Fix getParentDocument by parent first

--- src/helpers.js
+++ src/helpers.js
@@ -10,19 +10,18 @@ export const isRootFolder = (path) => {
 };
 
 // get parent or root or top workspaces document
-export const getParentDocument = (app, id, doc) => {
-  let parentQuery = Promise.resolve();
+export const getParentDocument = async (app, id, doc) => {
+  // get by the parent if provided
+  if (doc.parent) {
+    const svcDocuments = app.service('documents');
+    return svcDocuments.get(doc.parent);
+  }
   // get by id and type
   if (id) {
     const svcDocuments = app.service(plural(doc && doc.type || 'document'));
     return svcDocuments.get(id, {
       query: { $select: 'parent,*' }
     }).then(doc => doc && doc.parent);
-  }
-  // get by the parent id
-  if (doc.parent) {
-    const svcDocuments = app.service('documents');
-    return svcDocuments.get(doc.parent);
   }
   // get the root folder or workspaces root
   if (!isRootFolder(doc.path)) {
@@ -33,7 +32,7 @@ export const getParentDocument = (app, id, doc) => {
       return svcFolder.get(null, { query: { path : '/' } });
     }
   }
-  return Promise.resolve();
+  return null;
 };
 
 // generate a short typed name for a document
