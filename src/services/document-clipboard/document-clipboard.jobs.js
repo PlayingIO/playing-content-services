1ff50cfe70e2960cfce24fe3f8ed2538e1d6239d
forafish <fora.luo@gmail.com>
2018-07-04 23:58:15 +0800

    add document-clipboards jobs for fanout operations

---
+++ src/services/document-clipboard/document-clipboard.jobs.js
@@ -0,0 +1,31 @@
+import assert from 'assert';
+import makeDebug from 'debug';
+import fp from 'mostly-func';
+import { copyDocument, moveDocument } from '../../helpers';
+
+const debug = makeDebug('playing:content-services:documents/clipboards:jobs');
+
+export default function (app, options) {
+  const agenda = app.agenda;
+  const lockLifetime = options.agenda && options.agenda.lockLifetime || 300 * 1000;
+  assert(agenda, 'agenda not configured properly, check your app');
+
+  // fanout the operations to target document
+  agenda.define('fanout_operation', { lockLifetime }, function (job, next) {
+    debug('>>> fanout_operation', job.attrs.data);
+    const { operation, documents, target } = job.attrs.data;
+    if (operation && documents && target && documents.length > 0) {
+      const operations = fp.map(doc => {
+        switch (operation) {
+          case 'copyDocuments': return copyDocument(app, doc, target);
+          case 'moveDocuments': return moveDocument(app, doc, target);
+          default: Promise.resolve();
+        }
+      }, documents);
+      Promise.all(operations).then(next);
+    } else {
+      console.error('fanout_operation job is not provided:', job.attrs.data);
+      next();
+    }
+  });
+}
