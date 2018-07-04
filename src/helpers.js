226757018c79a24218f312c858aa0799220380d0
forafish <fora.luo@gmail.com>
2018-07-04 23:40:44 +0800

    add fanoutOperations

--- src/helpers.js
+++ src/helpers.js
@@ -10,6 +10,36 @@ export const getMetaSubtypes = (doc) => {
 };
 
 /**
+ * Get children documents
+ */
+export const getChildren = async (app, parents) => {
+  const svcDocuments = app.service('documents');
+  const children = await svcDocuments.find({
+    query: { parent: { $in: parents } },
+    paginate: false
+  });
+  return fp.groupBy(fp.prop('parent'), children);
+};
+
+/**
+ * Fanout move/copy operation to children documents
+ */
+export const fanoutOperations = async (app, documents, operation, target) => {
+  // get children documents with skip/limit
+  const parents = fp.map(fp.prop('id'), documents);
+  const childrens = await getChildren(app, parents);
+  if (!fp.isEmpty(childrens)) {
+    fp.forEachObjIndexed((children, parent) => {
+      app.agenda.now('fanout_operation', {
+        operation, documents: children, target: parent
+      });
+    }, childrens);
+    // process next batch of children
+    await fanoutOperations(app, path, operation, target);
+  }
+};
+
+/**
  * Copy documents to target
  */
 export const copyDocument = async (app, doc, target) => {
