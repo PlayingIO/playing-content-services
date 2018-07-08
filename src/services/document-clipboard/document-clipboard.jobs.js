c1780664265a819855ab4d58c54cee3290c30b1e
forafish <fora.luo@gmail.com>
2018-07-09 05:02:49 +0800

    fanoutDocuments without target

--- src/services/document-clipboard/document-clipboard.jobs.js
+++ src/services/document-clipboard/document-clipboard.jobs.js
@@ -10,15 +10,15 @@ export default function (app, options) {
   const lockLifetime = options.agenda && options.agenda.lockLifetime || 300 * 1000;
   assert(agenda, 'agenda not configured properly, check your app');
 
-  // fanout the operations to target document
+  // fanout the operations to copy/move documents
   agenda.define('fanout_documents', { lockLifetime }, function (job, next) {
     debug('>>> fanout_documents', job.attrs.data);
-    const { operation, documents, target } = job.attrs.data;
-    if (operation && documents && target && documents.length > 0) {
+    const { operation, documents } = job.attrs.data;
+    if (operation && documents && documents.length > 0) {
       const operations = fp.map(doc => {
         switch (operation) {
-          case 'copyDocuments': return copyDocument(app, doc, target);
-          case 'moveDocuments': return moveDocument(app, doc, target);
+          case 'copyDocuments': return copyDocument(app, doc);
+          case 'moveDocuments': return moveDocument(app, doc);
           default: Promise.resolve();
         }
       }, documents);
