de53f58b5d9ef8b2901f6c7822ef2828f5524155
forafish <fora.luo@gmail.com>
2018-07-08 23:39:15 +0800

    move helper fanoutDocuments

--- src/helpers.js
+++ src/helpers.js
@@ -3,36 +3,6 @@ import path from 'path';
 import { plural } from 'pluralize';
 
 /**
- * Get children documents
- */
-export const getChildrens = async (app, parents) => {
-  const svcDocuments = app.service('documents');
-  const children = await svcDocuments.find({
-    query: { parent: { $in: parents } },
-    paginate: false
-  });
-  return fp.groupBy(fp.prop('parent'), children);
-};
-
-/**
- * Fanout move/copy operation to children documents
- */
-export const fanoutDocuments = async (app, documents, operation, target) => {
-  // get children documents with skip/limit
-  const parents = fp.map(fp.prop('id'), documents);
-  const childrens = await getChildrens(app, parents);
-  if (!fp.isEmpty(childrens)) {
-    for (const [parent, children] of Object.entries(childrens)) {
-      app.agenda.now('fanout_documents', {
-        operation, documents: children, target: target
-      });
-      // process next batch of children
-      await fanoutDocuments(app, children, operation, parent);
-    }
-  }
-};
-
-/**
  * Copy documents to target
  */
 export const copyDocument = async (app, doc, target) => {
